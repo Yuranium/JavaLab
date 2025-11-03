6e8612389464be756cdee4f7bd203a72d244bab3
Yuranium <dolazifet@gmail.com>
2025-11-04 02:23:21 +0300

    feature: Working on application security

---
+++ UserService/src/main/java/com/yuranium/userservice/config/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.yuranium.userservice.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig
+{
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception
+    {
+        return security.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/validate")
+                        .permitAll()
+                        .requestMatchers("/api/v1/user/**")
+                        .authenticated())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
--- UserService/src/main/java/com/yuranium/userservice/service/AuthService.java
+++ UserService/src/main/java/com/yuranium/userservice/service/AuthService.java
@@ -5,6 +5,7 @@ import com.yuranium.userservice.models.entity.AuthEntity;
 import com.yuranium.userservice.models.entity.UserEntity;
 import com.yuranium.userservice.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,13 +15,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthService
 {
+    private final PasswordEncoder passwordEncoder;
+
     private final AuthRepository authRepository;
 
     @Transactional
     public AuthEntity setAuthForLocalUser(UserEntity user, UserRequestDto userDto)
     {
         AuthEntity authEntity = new AuthEntity();
-        authEntity.setPassword(userDto.password());
+        authEntity.setPassword(passwordEncoder.encode(userDto.password()));
         authEntity.setUser(user);
         authRepository.save(authEntity);
         user.setAuthMethods(List.of(authEntity));
