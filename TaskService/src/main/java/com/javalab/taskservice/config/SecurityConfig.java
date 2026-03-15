e1ab57513775a149770f8a776d16233335ffb57a
Yuranium <dolazifet@gmail.com>
2026-03-15 23:49:01 +0300

    feature: Make TaskService as a resource server

--- TaskService/src/main/java/com/javalab/taskservice/config/SecurityConfig.java
+++ TaskService/src/main/java/com/javalab/taskservice/config/SecurityConfig.java
@@ -3,11 +3,19 @@ package com.javalab.taskservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -24,14 +32,36 @@ public class SecurityConfig
                         .requestMatchers(HttpMethod.POST, "/api/v1/task/category").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PATCH, "/api/v1/task/category").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/task/category").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PATCH, "/api/v1/task/**/starter-code").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/v1/task/**/test-case").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PATCH, "/api/v1/task/**/test-case/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/v1/task/**/test-case/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/task/*/starter-code").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/task/*/test-case").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/task/*/test-case/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/task/*/test-case/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
+                .oauth2ResourceServer(auth -> auth.jwt(Customizer.withDefaults()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter()
+    {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setPrincipalClaimName("sub");
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            if (realmAccess == null) return Collections.emptySet();
+
+            Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+            if (roles == null) return Collections.emptySet();
+
+            return roles.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        });
+
+        return converter;
     }
 }
