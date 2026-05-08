042d5e5ccff4769d1da1116d78f626556e164d1a
Yuranium <dolazifet@gmail.com>
2026-05-08 21:34:56 +0300

    feature: add OAuth2 resource server support and configure JWT authentication

--- NotificationService/src/main/java/com/wenn/notificationservice/config/SecurityConfig.java
+++ NotificationService/src/main/java/com/wenn/notificationservice/config/SecurityConfig.java
@@ -2,11 +2,19 @@ package com.wenn.notificationservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebMvc
@@ -21,6 +29,28 @@ public class SecurityConfig
                 )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(auth -> auth.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter()
+    {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setPrincipalClaimName("sub");
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            if (realmAccess == null) return Collections.emptySet();
+
+            Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+            if (roles == null) return Collections.emptySet();
+
+            return roles.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        });
+
+        return converter;
     }
 }
